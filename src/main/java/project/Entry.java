52e4a3135c4ff391dcd54857d76ce0f61812a14f
irinamavrina <irina.mavrina@duallab.com>
2023-04-29 13:32:44 +0300

    Update Entry 4.0

--- src/main/java/project/Entry.java
+++ src/main/java/project/Entry.java
@@ -33,7 +33,7 @@ public class Entry {///
 	}//
 
 	public boolean isMoreEfficient(Entry e) {
-		LocalTime d1 = this.departureTime;
+		LocalTime d1 = this.departureTime;//
 		LocalTime d2 = e.departureTime;
 		LocalTime a1 = this.arrivalTime;
 		LocalTime a2 = e.arrivalTime;
